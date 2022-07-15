089ef1f3c0d0f2030f13f6b5ee0583ef66489267
Ian Pan <ianpan@amazon.com>
2022-07-15 15:10:06 -0700

    Use Python 🐍 script to remove all CPP files in subfolders

--- script.py
+++ script.py
@@ -4,10 +4,11 @@ for dir_name in os.listdir("src"):
     readme_content: str = ""
     for file_name in os.listdir(os.path.join("src", dir_name)):
         if file_name.endswith(".cpp"):
-            with open(os.path.join("src", dir_name, file_name), 'r') as f:
-                readme_content = f.read()
-            break
+            os.remove(os.path.join("src", dir_name, file_name))
+            # with open(os.path.join("src", dir_name, file_name), 'r') as f:
+            #     readme_content = f.read()
+            # break
 
-    appendage: str = "\n## My C++ Solution:\n\n```cpp\n" + readme_content + "```"
-    with open(os.path.join("src", dir_name, "README.md"), 'a') as f:
-        f.write(appendage)
+    # appendage: str = "\n## My C++ Solution:\n\n```cpp\n" + readme_content + "```"
+    # with open(os.path.join("src", dir_name, "README.md"), 'a') as f:
+    #     f.write(appendage)
--- src/Contains Duplicate/containsDuplicate.cpp
+++
@@ -1,7 +0,0 @@
-class Solution {
- public:
-  bool containsDuplicate(vector<int> &nums) {
-    unordered_set<int> s(nums.begin(), nums.end());
-    return nums.size() > s.size();
-  }
-};
